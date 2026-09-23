0fca16a20b4a9740afd5019cfbd61129cc416994
Manisha <manisha7kumari7777@gmail.com>
2026-09-23 15:05:18 +0000

    Path Parameter

--- fastapi_blog/main.py
+++ fastapi_blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 #for HTML response
 from fastapi.responses import HTMLResponse
@@ -37,6 +37,24 @@ def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home Page"})
 
 
+@app.get("/posts/{post_id}", include_in_schema=False)
+def get_post_page(request: Request, post_id: int):
+    for post in posts:
+        title = post["title"][0:50]  # Get the first 10 characters of the title
+        if post["id"] == post_id:
+            return templates.TemplateResponse(request, "post.html", {"post": post, "title": title})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
 @app.get("/api/posts")
 def get_posts():
     return posts    
+
+
+# `post_id` is a path parameter, and the `int` annotation type-casts it from the URL string.
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post["id"]==post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
